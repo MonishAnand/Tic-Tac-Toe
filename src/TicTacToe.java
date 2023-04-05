8bee21d0186599ed2836d38a340e00639888d762
monish <monishanand.j@gmail.com>
2023-04-05 13:08:02 +0530

    buttons added and 3*3 grid obtained

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -24,11 +24,23 @@ public class TicTacToe implements ActionListener {
         text.setText("Tic-Tac-Toe");
         text.setOpaque(true);
 
+        button.setLayout(new GridLayout(3,3));
+        button.setBackground(Color.cyan);
+
+        for(int i =0;i<9;i++){
+            buttons [i]=new JButton();
+            button.add(buttons[i]);
+            buttons[i].setFont(new Font("MV Boli",Font.ITALIC,120));
+            buttons[i].setFocusable(false);
+            buttons[i].addActionListener(this);
+        }
+
         title.setLayout(new BorderLayout());
         title.setBounds(0,0,800,100);
 
         title.add(text);
         f.add(title,BorderLayout.NORTH);
+        f.add(button);
     }
 
     @Override
